64f0406986ea25cbe2ee843a26c40dfb1b32e492
Aditya Rathi <adityarathi95@gmail.com>
2016-03-31 02:26:48 +0530

    - Select Screen - Custom View made - added drawables

--- app/src/main/java/com/waltado/lynknow/activities/MainActivity.java
+++ app/src/main/java/com/waltado/lynknow/activities/MainActivity.java
@@ -1,9 +1,13 @@
 package com.waltado.lynknow.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.waltado.lynknow.R;
+import com.waltado.lynknow.customViews.LatoFontTextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
--- app/src/main/java/com/waltado/lynknow/activities/RegisterActivity.java
+++ app/src/main/java/com/waltado/lynknow/activities/RegisterActivity.java
@@ -1,9 +1,12 @@
 package com.waltado.lynknow.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.waltado.lynknow.R;
+import com.waltado.lynknow.customViews.LatoFontTextView;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -11,5 +14,13 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        ((LatoFontTextView)findViewById(R.id.proceedView)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, SelectActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
